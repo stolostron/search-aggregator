2463bda8e8f21a23df7ba0e1e138663fe6d9180d
XAVIER R. DHARMAIYAN <xrdharma@us.ibm.com>
2019-04-11 13:07:43 -0400

    added client to go ssl if REDIS_SSH_PORT is set (#24)
    * added client to go ssl if REDIS_SSH_PORT is set
    
    * remove duplicated code

--- pkg/dbconnector/redisgraphConnector.go
+++ pkg/dbconnector/redisgraphConnector.go
@@ -9,7 +9,10 @@ The source code for this program is not published or otherwise divested of its t
 package dbconnector
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
+	"io/ioutil"
 	"os"
 
 	"github.com/golang/glog"
@@ -34,6 +37,8 @@ func Init() {
 }
 
 func connectRedisClient() error {
+	var redisconn redis.Conn
+	var rediserr error
 	redisHost := os.Getenv("REDIS_HOST")
 	if redisHost == "" {
 		redisHost = "localhost"
@@ -42,29 +47,62 @@ func connectRedisClient() error {
 	if redisPort == "" {
 		redisPort = "6379"
 	}
-	glog.Info("Initializing new Redis client with redisHost: ", redisHost, " redisPort: ", redisPort)
 
-	conn, err := redis.Dial("tcp", redisHost+":"+redisPort)
-	if err != nil {
-		glog.Error("Error connecting redis host.")
-		return err
+	redisSSHPort := os.Getenv("REDIS_SSH_PORT")
+	if redisSSHPort != "" {
+		glog.Info("Initializing new Redis SSH client with redisHost: ", redisHost, " redisSSHPort: ", redisSSHPort)
+		caCert, err := ioutil.ReadFile("./sslcert/redis.crt")
+		if err != nil {
+			glog.Error("Error loading TLS certificate. Redis Certificate must be mounted at ./sslcert/redis.crt")
+			glog.Error(err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		tlsconf := &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+			RootCAs: caCertPool,
+		}
+		redisconn, rediserr = redis.Dial("tcp", redisHost+":"+redisSSHPort,
+			redis.DialTLSConfig(tlsconf),
+			redis.DialUseTLS(true))
+		if rediserr != nil {
+			glog.Error("Error connecting redis using SSH .")
+			return rediserr
+		}
+
+	} else {
+		glog.Info("Initializing new Redis client with redisHost: ", redisHost, " redisPort: ", redisPort)
+
+		redisconn, rediserr = redis.Dial("tcp", redisHost+":"+redisPort)
+		if rediserr != nil {
+			glog.Error("Error connecting redis host.")
+			return rediserr
+		}
+
 	}
-
 	// If we have a REDIS_PASSWORD, we'll try to authenticate the Redis client.
 	redisPassword := os.Getenv("REDIS_PASSWORD")
 	if redisPassword != "" {
 		glog.Info("Authenticating Redis client.")
-		if _, err := conn.Do("AUTH", redisPassword); err != nil {
+		if _, rediserr := redisconn.Do("AUTH", redisPassword); rediserr != nil {
 			glog.Error("Error authenticating Redis client.")
-			return err
+			return rediserr
 		}
 	} else {
 		glog.Warning("REDIS_PASSWORD wasn't provided. Attempting to communicate without authentication.")
 	}
-
-	graph = rg.Graph{}.New("icp-search", conn)
+	graph = rg.Graph{}.New("icp-search", redisconn)
 	client = &DbClient{
-		Conn:  conn,
+		Conn:  redisconn,
 		Graph: graph,
 	}
 
