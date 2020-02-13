867ab2ed5edd898d096e039e8d0625c54144d267
Jorge Padilla <jpadilla@us.ibm.com>
2020-02-12 22:15:54 -0500

    update

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# build
-echo "building"
+
+echo " > Running build.sh"
+set -e
 CGO_ENABLED=0 go build -a -v -i -installsuffix cgo -ldflags '-s -w' -o search-aggregator ./
--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# install dependencies
-echo "installing dependencies for aggregator ..."
+echo " > Running install-dependencies.sh"
+set -e
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u github.com/golang/dep/cmd/dep
 dep ensure -v
