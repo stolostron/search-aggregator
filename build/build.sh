e89988ff65829939fe368792a53b3970c572c2c2
Marquis Hackett <Marquis Hackett>
2020-02-15 18:16:51 -0500

    rollback redisgraph-go client. they've updated to 2.0.0, and we were on @master, so our versions no longer matched

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,5 @@
 
 echo " > Running build.sh"
 set -e
-CGO_ENABLED=0 go build -a -v -i -installsuffix cgo -ldflags '-s -w' -o search-aggregator ./
+CGO_ENABLED=0 go build -a -v -i -installsuffix cgo -ldflags '-s -w' -o output/search-aggregator ./
+make docker/build
