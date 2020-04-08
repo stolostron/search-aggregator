402c968ab8cc6ed141e877768e41f7802f4d6aa5
Jorge Padilla <jpadilla@us.ibm.com>
2020-04-08 17:09:12 -0400

    Remove ciphers

--- main.go
+++ main.go
@@ -49,7 +49,6 @@ func main() {
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
 	}
