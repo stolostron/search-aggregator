f4ee0a85564009e88f4c9211a7838fd15c260648
Jorge Padilla <jpadilla@us.ibm.com>
2020-04-08 16:54:37 -0400

    Remove vulnerable TLS cipher

--- main.go
+++ main.go
@@ -48,9 +48,7 @@ func main() {
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
-			// TODO: Update list with acceptable FIPS ciphers.
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
