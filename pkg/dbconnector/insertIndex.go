d0a0dec3af4e33784dc4bc4d99a5b86d2f6442ff
Sherin Varughese <sherin.a.varughese@gmail.com>
2020-09-03 16:31:40 -0400

    reduce resyncs (#82)
    Master PR: https://github.com/open-cluster-management/search-aggregator/pull/77

--- pkg/dbconnector/insertIndex.go
+++ pkg/dbconnector/insertIndex.go
@@ -13,6 +13,7 @@ var ExistingIndexMap = make(map[string]bool)
 
 // GetIndexes - returns map to hold all resource kinds that have index built in redisgraph
 func GetIndexes() {
+	glog.V(4).Info("Fetching indexes")
 	resp, err := Store.Query("MATCH (n) RETURN distinct labels(n)")
 	if err == nil {
 		var ExistingIndexMapMutex = sync.RWMutex{}
@@ -44,7 +45,9 @@ func GetIndexes() {
 
 // Given a resource, inserts index on resource uid into redisgraph.
 func insertIndex(kind, property string) error {
+	glog.V(4).Info("Inserting index")
 	query := SanitizeQuery("CREATE INDEX ON :%s(%s)", kind, property) //CREATE INDEX ON :Pod(_uid)"
 	_, err := Store.Query(query)
+	glog.V(4).Info("Insert index query: ", query)
 	return err
 }
