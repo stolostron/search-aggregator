bafaa60f5a79b771ec28aacf9e790d7f1514e954
Jorge Padilla <jpadilla@redhat.com>
2020-11-10 22:24:16 -0500

    Get conditions from ManagedCluster object

--- pkg/clustermgmt/clusterWatch_test.go
+++ pkg/clustermgmt/clusterWatch_test.go
@@ -1,35 +1,51 @@
-/*
-IBM Confidential
-OCO Source Materials
-(C) Copyright IBM Corporation 2019 All Rights Reserved
-The source code for this program is not published or otherwise divested of its trade secrets,
-irrespective of what has been deposited with the U.S. Copyright Office.
-*/
 // Copyright (c) 2020 Red Hat, Inc.
 
 package clustermgmt
 
 import (
 	"testing"
+
+	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
+	clusterv1beta1 "github.com/open-cluster-management/multicloud-operators-foundation/pkg/apis/cluster/v1beta1"
+	utils "github.com/open-cluster-management/search-aggregator/pkg/utils"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestTransformCluster(t *testing.T) {
-	// testcluster := clusterregistry.Cluster{}
-	// testclusterstatus := mcmapi.ClusterStatus{}
-	// utils.UnmarshalFile("cluster.json", &testcluster, t)
-	// utils.UnmarshalFile("clusterstatus.json", &testclusterstatus, t)
-	// result := transformCluster(&testcluster, &testclusterstatus)
-	// assert.Equal(t, result.Kind, "Cluster", "Test Kind")
-	// assert.Equal(t, result.ResourceString, "clusters", "Test ResourceString")
-	// assert.Equal(t, result.UID, "1baa5f8a-758f-11e9-9527-667a72062d69", "Test UID")
-	// assert.Equal(t, result.Properties["name"], "xav-cluster", "Test Name")
-	// assert.Equal(t, result.Properties["namespace"], "xav-cluster-ns", "Test namespace")
-	// assert.Equal(t, (result.Properties["label"]).(map[string]interface{})["cloud"], "IBM", "Test label")
-	// assert.Equal(t, result.Properties["created"], "2019-05-13T14:55:11Z", "Test created")
-	// assert.Equal(t, result.Properties["consoleURL"], "https://222.222.222.222:8443", "Test consoleURL")
-	// assert.Equal(t, result.Properties["cpu"], int64(24), "Test cpu")
-	// assert.Equal(t, result.Properties["memory"], "98143Mi", "Test memory")
-	// assert.Equal(t, result.Properties["nodes"], int64(3), "Test nodes")
-	// assert.Equal(t, result.Properties["storage"], "60Gi", "Test storage")
+func Test_transformManagedCluster(t *testing.T) {
+	managedCluster := clusterv1.ManagedCluster{}
+	utils.UnmarshalFile("managedCluster.json", &managedCluster, t)
 
+	result := transformManagedCluster(&managedCluster)
+
+	assert.Equal(t, "Amazon", (result.Properties["label"]).(map[string]interface{})["cloud"], "Test property: label")
+	assert.Equal(t, "internal.open-cluster-management.io", result.Properties["apigroup"], "Test property: apigroup")
+	assert.Equal(t, "Cluster", result.Kind, "Test property: Kind")
+	assert.Equal(t, "managed-cluster-01", result.Properties["name"], "Test property: name")
+	assert.Equal(t, "2020-11-10T22:46:08Z", result.Properties["created"], "Test property: created")
+	assert.Equal(t, int64(36), result.Properties["cpu"], "Test property: cpu")
+	assert.Equal(t, "144576Mi", result.Properties["memory"], "Test property: memory")
+	assert.Equal(t, "v1.18.3+6c42de8", result.Properties["kubernetesVersion"], "Test property: kubernetesVersion")
+
+	assert.Equal(t, "True", result.Properties["HubAcceptedManagedCluster"], "Test property: HubAcceptedManagedCluster")
+	assert.Equal(t, "True", result.Properties["ManagedClusterJoined"], "Test property: ManagedClusterJoined")
+	assert.Equal(t, "True", result.Properties["ManagedClusterConditionAvailable"], "Test property: ManagedClusterConditionAvailable")
+
+	assert.Equal(t, "managedclusterinfos", result.ResourceString, "Test property: ResourceString")
+	assert.Equal(t, "cluster__managed-cluster-01", result.UID, "Test property: UID")
+}
+
+func Test_transformManagedClusterInfo(t *testing.T) {
+	managedClusterInfo := clusterv1beta1.ManagedClusterInfo{}
+	utils.UnmarshalFile("managedClusterInfo.json", &managedClusterInfo, t)
+
+	result := transformManagedClusterInfo(&managedClusterInfo)
+
+	assert.Equal(t, "internal.open-cluster-management.io", result.Properties["apigroup"], "Test property: apigroup")
+	assert.Equal(t, result.Kind, "Cluster", "Test Kind")
+	assert.Equal(t, "managed-cluster-01", result.Properties["name"], "Test property: name")
+	assert.Equal(t, "https://console-openshift-console.apps.base-host-name.com", result.Properties["consoleURL"], "Test property: consoleURL")
+	assert.Equal(t, int64(6), result.Properties["nodes"], "Test property: nodes")
+
+	assert.Equal(t, "managedclusterinfos", result.ResourceString, "Test property: ResourceString")
+	assert.Equal(t, "cluster__managed-cluster-01", result.UID, "Test property: UID")
 }
