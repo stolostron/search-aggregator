51a440f0c67396e0d04aaa77e0cda4b0f04d23ef
Sherin Varughese <shvarugh@redhat.com>
2021-04-29 11:57:01 -0400

    check for redis pod before readiness

--- pkg/handlers/healthProbes.go
+++ pkg/handlers/healthProbes.go
@@ -14,9 +14,13 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/golang/glog"
+	"github.com/open-cluster-management/search-aggregator/pkg/config"
 	db "github.com/open-cluster-management/search-aggregator/pkg/dbconnector"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // LivenessProbe is used to check if this service is alive.
@@ -27,8 +31,18 @@ func LivenessProbe(w http.ResponseWriter, r *http.Request) {
 
 // ReadinessProbe checks if Redis is available.
 func ReadinessProbe(w http.ResponseWriter, r *http.Request) {
-	glog.V(2).Info("readinessProbe - Checking Redis connection.")
 
+	glog.V(2).Info("readinessProbe - Checking Redis connection.")
+	ns := os.Getenv("POD_NAMESPACE")
+	if ns == "" {
+		ns = "open-cluster-management"
+	}
+	options := metav1.GetOptions{}
+	_, err := config.GetKubeClient().CoreV1().Pods(ns).Get("search-redisgraph-0", options)
+	if err != nil && errors.IsNotFound(err) {
+		glog.V(2).Info("Redisgraph pod is not present - will re-check once it is enabled.")
+		return
+	}
 	// Go straight to the pool's Dial because we don't actually want to play by the pool's
 	// rules here - just want a connection unrelated to all the other ones,
 	conn, err := db.Pool.Dial()
