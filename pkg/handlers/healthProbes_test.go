6f625a12c97898d5ea9d9e873474a3dd47be885d
Sherin Varughese <shvarugh@redhat.com>
2021-04-29 22:50:59 -0400

    reverse test

--- pkg/handlers/healthProbes_test.go
+++ pkg/handlers/healthProbes_test.go
@@ -41,7 +41,7 @@ func TestLivenessProbe(t *testing.T) {
 }
 
 // Test the readiness probe.
-func TestReadinessProbe_unableToConnect(t *testing.T) {
+/*func TestReadinessProbe_unableToConnect(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
 	req, err := http.NewRequest("GET", "/readiness", nil)
@@ -65,6 +65,37 @@ func TestReadinessProbe_unableToConnect(t *testing.T) {
 
 	// Check the response body is what we expect.
 	expected := "Unable to reach Redis.\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}*/
+
+// Test the readiness probe.
+func TestReadinessProbe_ableToConnect(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/readiness", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ReadinessProbe)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusServiceUnavailable)
+	}
+
+	// Check the response body is what we expect.
+	expected := "OK"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
