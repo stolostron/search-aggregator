91dad20cc858ee7914b16e192eac8776e03c5d61
Sherin Varughese <sherin.a.varughese@gmail.com>
2021-04-30 09:11:55 -0400

    disable readiness check (#124)
    * disable readiness check
    
    * reverse test

--- pkg/handlers/healthProbes.go
+++ pkg/handlers/healthProbes.go
@@ -16,7 +16,6 @@ import (
 	"net/http"
 
 	"github.com/golang/glog"
-	db "github.com/open-cluster-management/search-aggregator/pkg/dbconnector"
 )
 
 // LivenessProbe is used to check if this service is alive.
@@ -31,15 +30,15 @@ func ReadinessProbe(w http.ResponseWriter, r *http.Request) {
 
 	// Go straight to the pool's Dial because we don't actually want to play by the pool's
 	// rules here - just want a connection unrelated to all the other ones,
-	conn, err := db.Pool.Dial()
-	if err != nil {
-		// Respond with error.
-		glog.Warning("Unable to reach Redis.")
-		http.Error(w, "Unable to reach Redis.", 503)
-		return
-	}
+	// conn, err := db.Pool.Dial()
+	// if err != nil {
+	// 	// Respond with error.
+	// 	glog.Warning("Unable to reach Redis.")
+	// 	http.Error(w, "Unable to reach Redis.", 503)
+	// 	return
+	// }
 
-	defer conn.Close()
+	// defer conn.Close()
 	// Respond with success
 	fmt.Fprint(w, "OK")
 }
